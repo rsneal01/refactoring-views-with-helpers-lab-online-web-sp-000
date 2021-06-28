4c8761c21186b4cbb71bdae8d62e98ab97414850
rsneal01 <rsneal01@gmail.com>
2021-06-28 10:22:12 -0700

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,13 @@
 module ArtistsHelper
+
+    def display_artist(song)
+        # @song = Song.find(params[:id])
+        @artist = song.artist
+        if @artist == nil
+            link_to "Add Artist", edit_song_path(song)
+        else
+            link_to @artist.name, artist_path(@artist)
+        end
+    end
+
 end
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,13 @@
 module SongsHelper
 
+    def display_song(song)
+        # @song = Song.find(params[:id])
+        # @artist = @song.artist
+        if song.artist == nil
+            link_to "Add Artist", edit_song_path(song)
+        else
+            link_to song.title, song_path(song)
+        end
+    end
+
 end
